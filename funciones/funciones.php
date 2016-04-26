fa62e6c55b5b11dd2bf99213e18da7cf8702fafb
javier <javier.iesn@gmail.com>
2016-04-26 11:13:22 +0000

    Actualización 2016-04-26:11:13

--- funciones/funciones.php
+++ funciones/funciones.php
@@ -14,21 +14,88 @@
    		}
    	}
    }
-   function sumaMatrices($a,$b) {
-      //$a y $b debería ser matrices bidimensionales
-      //......
-      for($i=0;$i < count($a); $i++) {
-         for($j=0;$j < count($a[0]);$j++)  {
-            $c[$i][$j]=$a[$i][$j]+$b[$i][$j];
+   function sumaRestaMatrices($a,$b,$op=1) {
+   	  //Esta función suma o resta las matrices $a y $b en funcioón del tercer parámetro opcional
+   	  //$op que si vale 1 se haría la suma y sino la resta
+   	  //en el caso de que la matriz $c devuelta sea vacía una de dos o $a y $b eran matrices vacías
+   	  //o algo ha ido mal
+   	  $c=array();
+   	  
+   	  if (gettype($a) != "array" || gettype($b) != "array") {	
+   	     return $c; 	  
+   	  }
+   	  if (count($a)!=count($b) or count($a[0])!=count($b[0])) {
+   	  	 return $c;
+   	  }
+   	  for($i=0;$i<count($a);$i++) {
+   	  	 for($j=0; $j < count($a[0]); $j++) {
+   	  	 	if ($op == 1 ) { // sumar
+   	  	 		$c[$i][$j]=$a[$i][$j]+$b[$i][$j];
+   	  	 	} else {
+   	  	 		$c[$i][$j]=$a[$i][$j]-$b[$i][$j];
+   	  	 	}
+   	  	 }
+   	  }
+   	  return $c;
+   }
+   function letraNif($ndni) {
+   	  $letras=array("T","R","W","A","G","M","Y","F","P","D","X","B","N","J","Z","S","Q","V","H","L","C","K","E");
+   	  return $letras[$ndni % 23];
+   }
+   function digitosControl($entidad,$oficina,$cuenta) {
+        //entidad, oficina y cuenta pueden ser números enteros o cadenas de caracteres
+        // representando un  número de cuenta bancaria válido
+        
+   	  $pesos=array(1,2,4,8,5,10,9,7,3,6);
+   	  $entidadOficina="00"."$entidad"."$oficina";
+   	  $cuenta="$cuenta";
+
+   	  $suma1=$suma2=0;
+   	  for($i=0;$i < count($pesos); $i++) {
+   	  	  $suma1=$suma1+$pesos[$i]*$entidadOficina[$i];
+   	  	  $suma2=$suma2+$pesos[$i]*$cuenta[$i];
+   	  }
+   	  $resto1=11-$suma1 % 11;
+   	  $resto2=11-$suma2 % 11;
+   	  if ($resto1 == 10) {
+   	     $resto1=1;
+   	  }  elseif ( $resto1 == 11 ) {
+   	     $resto1=0;
+   	  }
+   	  if ($resto2 == 10) {
+   	     $resto2=1;
+   	  }  elseif ( $resto2 == 11 ) {
+   	     $resto2=0;
+   	  }
+   	  return "$resto1"."$resto2";
+   }
+   
+   function cuentaLetra($cadena,$letra) {
+      $n=0; // al final tendrá el número de apariciones de $letra dentro de $cadena
+      for($i=0;$i<strlen($cadena);$i++) {
+         if ($cadena[$i]==$letra) {
+            $n++; //$n=$n+1;
          }
       }
-      return $c;
+      
+      return $n;
    }
-   function suma($a,$b) {
-      return $a+$b;
+   
+   
+   
+   
+   $a=5;
+   echo $a[0];
+   
+   
+   
+   $d=array(array(1,2),array(3,4));
+   $e=array(array(5,6),array(7,8));
+   $suma=sumaRestaMatrices($d,$e);
+   $resta=sumaRestaMatrices($d,$e,0);
+   if (count($suma) == 0) {
+   	 echo "ha habido un error sumando las matries.";
    }
-   $a=array(array(1,2,3),array(4,5,6));
-   $b=array(array(7,8,9),array(10,11,12));
-   $resultado=sumaMatrices($a,$b);
+   
    
 ?>
